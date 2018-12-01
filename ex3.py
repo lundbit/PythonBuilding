2d7e1c02e182d7c3bfad04b0216c5b04ad99fb2c
Brad Lund <32400736+lundbit@users.noreply.github.com>
2018-12-01 14:13:23 -0700

    Today's practice in Python

---
+++ ex3.py
@@ -0,0 +1,36 @@
+
+# This prints the intention to count chickens
+print("I will now count my chickens:")
+
+# This adds Hens, and notice that the result is 30, we divide 30/6 first then add to 25
+print("Hens", 25 + 30 / 6)
+
+# This uses 75 modulo 4 -modulo has same precedence as multiplication, so it becomes 100-3
+print("Roosters", (100 - 25 * 3 % 4)*1.0)
+
+# We are now counting eggs
+print("Now I will count the eggs:")
+
+# more operation practice...4%2 is zero, do division first and you get 7-.25
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# posing question
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# presenting operation to obtain boolean
+print(3 + 2 < 5 - 7)
+
+# presenting math questions using operations after the text, divided by commas
+print("What is 3 + 2?", (3 + 2)*1.0)
+print("What is 5 - 7?", (5 - 7)*1.0)
+
+# printing observation
+print("Oh, that is why it is False.")
+
+# continuing boolean inquiries
+print("How about some more.")
+
+# text and operation separated by comma, resulting in a boolean value
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3extra.py
@@ -0,0 +1,6 @@
+# this program adds things together that I need
+
+print("My wife and I have six kids in the house.")
+print("Total people in house:", 6 + 2)
+print("My wife and I are going out to eat and our oldest Daughter is eating with a friend.")
+print("Total left in the house who we need to plan dinner for:", 8 - 3)
